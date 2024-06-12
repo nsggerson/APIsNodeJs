e77cd53060b7580a7cc78acf07a0ced9d1a6f7ed
nsggerson <nsggerson@gmail.com>
2024-06-12 19:01:55 -0300

    Desenvolvido a função que obtem uma porta disponivel, caso não encontre pega a 3000

--- server.js
+++ server.js
@@ -6,16 +6,17 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
+const { parse } = require('path');
 //const { title } = require('process');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port',port);
 
 const server = http.createServer(app);
 const router = express.Router();
 
-//http:/
+//http://localhost:3000
 const route = router.get('/', (req, res, next)=>{
     res.status(200).send({
         title: "Node Store API",
@@ -25,5 +26,19 @@ const route = router.get('/', (req, res, next)=>{
 
 app.use('/', route);
 server.listen(port);
+
+//Função normalize procura uma porta disponível se não hover pega a  3000
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+    return false;
+}
 
 console.log('API rodando na porta '+port);
