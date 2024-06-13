440543a869bdb714635250c8360c5add0cc2fd92
nsggerson <nsggerson@gmail.com>
2024-06-13 09:12:27 -0300

    Desenvolvido rotas do crud

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-//http://localhost:3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//http://localhost:3000/products
 const route = router.get('/', (req, res, next)=>{
     res.status(200).send({
         title: "Node Store API",
@@ -13,6 +17,30 @@ const route = router.get('/', (req, res, next)=>{
     });
 });
 
+//http://localhost:3000/products
+const create = router.post('/', (req, res, next)=>{
+    res.status(201).send(req.body);
+    console.log(req.body)
+});
+
+//http://localhost:3000/products/id
+const put = router.put('/:id', (req, res, next)=>{
+    const id = req.params.id;
+    res.status(200).send({
+        id:id,
+        item: req.body
+    });
+
+});
+
+//http://localhost:3000/products
+const del = router.delete('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
-console.log('APP Carregado...');
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
+
 module.exports = app;
