78737cd98e6765b08d733105f63d910a279e07d4
nsggerson <nsggerson@gmail.com>
2024-06-13 10:44:09 -0300

    Refatorando o controllers

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,26 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controllers'); 
 
-//http://localhost:3000/products
-router.post('/', (req, res, next)=>{
-    res.status(201).send(req.body);
-    console.log(req.body)
-});
-
-//http://localhost:3000/products/id
-router.put('/:id', (req, res, next)=>{
-    const id = req.params.id;
-    res.status(200).send({
-        id:id,
-        item: req.body
-    });
-
-});
-
-//http://localhost:3000/products
-router.delete('/', (req, res, next)=>{
-    res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
