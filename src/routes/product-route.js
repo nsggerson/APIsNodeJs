bee10258bbf439fff00474460b00f8d83cd8f919
nsggerson <nsggerson@gmail.com>
2024-06-18 12:38:18 -0300

    Modificado o params para body no delete

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -14,6 +14,6 @@ router.get('/admin/:id', controller.getById);
 
 router.post('/', controller.post);
 router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.delete('/', controller.delete);
 
 module.exports = router;
